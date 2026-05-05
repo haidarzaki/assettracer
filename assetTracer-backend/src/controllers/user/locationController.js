e10f5351b63d638137d12ccc15e9f47d05f79a90
dzakiyuddinhaidar@gmail.com <dzakiyuddinhaidar@gmail.com>
2026-05-05 10:13:20 +0700

    fix: export getLocations function

---
+++ assetTracer-backend/src/controllers/user/locationController.js
@@ -0,0 +1,11 @@
+import pool from "../../config/db";
+
+export const getLocations = async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM locations ORDER BY id ASC");
+    res.json(result.rows);
+  } catch (error) {
+    console.error("get location error: ", error);
+    res.status(500).json({ message: error.message });
+  }
+};
