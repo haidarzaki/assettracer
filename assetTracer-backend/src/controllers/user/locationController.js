eff36c9ca7ba84914160a1007c617144cc81df30
dzakiyuddinhaidar@gmail.com <dzakiyuddinhaidar@gmail.com>
2026-05-05 14:23:52 +0700

    feat: add CRUD API for locations

--- assetTracer-backend/src/controllers/user/locationController.js
+++ assetTracer-backend/src/controllers/user/locationController.js
@@ -9,3 +9,54 @@ export const getLocations = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const addLocation = async (req, res) => {
+  try {
+    const { name, address } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ message: "Nama lokasi wajib diisi" });
+    }
+
+    const newLocation = await pool.query(
+      `INSERT INTO locations (name, address) VALUES ($1, $2) RETURNING *`,
+      [name, address || null],
+    );
+
+    res.status(201).json({
+      message: "Lokasi berhasil ditambahkan",
+      data: newLocation.rows[0],
+    });
+  } catch (error) {
+    console.error("Add Location Error", error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateLocation = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, address } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ message: "Nama lokasi wajib diisi" });
+    }
+
+    const updatedLocation = await pool.query(
+      `UPDATE locations SET name = $1, address = $2, updated_at = NOW() WHERE id = $3 RETURNING *`,
+      [name, address || null, id],
+    );
+
+    if (updateLocation.rowCount === 0) {
+      return res.status(404).json({ message: "lokasi tidak ditemukan" });
+    }
+
+    res.json({
+      message: "Lokasi berhasil di update",
+      data: updatedLocation.rows[0],
+    });
+  } catch (error) {
+    console.error("Update Location Error", error);
+    res.status(500).json({ message: error.message });
+  }
+};
--- assetTracer-backend/src/routes/locationRoutes.js
+++ assetTracer-backend/src/routes/locationRoutes.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { getLocations } from "../controllers/user/locationController.js";
-import { verifyToken } from "../middlewares/authMiddleware.js";
+import {
+  addLocation,
+  getLocations,
+} from "../controllers/user/locationController.js";
+import { isAdmin, verifyToken } from "../middlewares/authMiddleware.js";
+import { updateItems } from "../controllers/user/itemsController.js";
 
 const router = express.Router();
 
 //Ambil lokasi
 router.get("/", verifyToken, getLocations);
+
+router.post("/add", verifyToken, isAdmin, addLocation);
+router.put("/:id", verifyToken, isAdmin, updateItems);
 
 export default router;
